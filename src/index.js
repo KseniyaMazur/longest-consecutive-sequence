1656e2bcea07f0685bf138a84f9296c6b18a2c48
KseniyaMazur <ksuha80018@gmail.com>
2018-03-02 14:01:27 +0300

    longest-consecutive-sequence/feature: implement longestConsecutiveLength

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = 
+function longestConsecutiveLength(array) {
+  let longestLength = 1;
+  let lengthArray = array.length;
+  let count = 1;
+  if (lengthArray < 1){
+    return 0;
+  }
+  array.sort((a,b) => { return a-b })
+  
+  for (let i = 0; i < lengthArray; i++){
+    if (array[i] === array [i + 1]){
+      continue;
+    }
+    if (array[i] + 1 === array[i+1]){
+      count++;
+    } else {
+      count = 1;
+    }
+    longestLength = Math.max(count, longestLength);
+  }
+  return longestLength;
+  //console.log(longestLength);
 }
+
+//longestConsecutiveLength([45,3,2,23,1234,0,1]);
